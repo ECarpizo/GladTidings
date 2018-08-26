81e485ab6e0030fa9e246c41b3a2e180de3d45fb
Eric C <ervcarpizo@gmail.com>
2018-08-26 01:28:30 -0400

    Fixed widths again. Updated homepage text

--- GladTidings/src/app/app.component.ts
+++ GladTidings/src/app/app.component.ts
@@ -33,4 +33,15 @@ export class AppComponent {
       this._loadingBar.stop();
     }
   }
+
+  onActivate(event) {
+    let scrollToTop = window.setInterval(() => {
+        let pos = window.pageYOffset;
+        if (pos > 0) {
+            window.scrollTo(0, pos - 20); // how far to scroll on each step
+        } else {
+            window.clearInterval(scrollToTop);
+        }
+    }, 16);
+  }
 }
